d5f6fe62ee11a4d3ed2994f1fde29fa76d4df21b
黄智 <huangzhi@emapgis.com>
2025-03-27 17:52:22 +0800

    添加vue方法自动引入以及解决ts和eslint报错提示

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,30 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import AutoImport from 'unplugin-auto-import/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 9001,
   },
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'pinia'],
+      dts: fileURLToPath(new URL('./auto-import.d.ts', import.meta.url)),
+      eslintrc: {
+        // 已存在文件设置默认 false，需要更新时再打开，防止每次更新都重新生成
+        enabled: false,
+        // 生成文件地址和名称
+        filepath: fileURLToPath(new URL('./.eslintrc-auto-import.json', import.meta.url)),
+        globalsPropValue: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
