968ebe4db4c43a665c717ab4c9a1fa898e9a369e
黄智 <1144061645@qq.com>
2025-03-27 21:40:09 +0800

    引入button组件

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from '@/components/button/button.vue'
+import type { IButtonInstance } from './components/button/types'
+
+const buttonRef = ref<IButtonInstance | null>(null)
+
+onMounted(() => {
+  if (buttonRef.value) {
+    console.log(buttonRef.value.ref)
+  }
+})
+</script>
 
 <template>
-  <div></div>
+  <div>
+    <Button type="primary" plain disabled ref="buttonRef">Test Button</Button>
+    <a href="#">123131</a>
+  </div>
 </template>
 
 <style scoped></style>
