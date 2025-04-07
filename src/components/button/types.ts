eb4465562a962628b39f04527f2befa8f525e728
黄智 <1144061645@qq.com>
2025-04-07 20:04:54 +0800

    修改类型书写的样式

--- src/components/button/types.ts
+++ src/components/button/types.ts
@@ -1,16 +1,16 @@
-export type buttonType = 'default' | 'primary' | 'success' | 'info' | 'warning' | 'danger'
-export type buttonSize = 'large' | 'small'
-export type nativeType = 'button' | 'submit' | 'reset'
+export type ButtonType = 'default' | 'primary' | 'success' | 'info' | 'warning' | 'danger'
+export type ButtonSize = 'large' | 'small'
+export type NativeType = 'button' | 'submit' | 'reset'
 
 export interface IButtonProps {
-  type?: buttonType
-  size?: buttonSize
+  type?: ButtonType
+  size?: ButtonSize
   plain?: boolean
   round?: boolean
   circle?: boolean
   disabled?: boolean
   // button 的原生属性
-  nativeType?: nativeType
+  nativeType?: NativeType
   autofocus?: boolean
 }
 export interface IButtonInstance {
