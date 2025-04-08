09daf6364065567907bd93e07ea4d537b6820367
黄智 <1144061645@qq.com>
2025-04-08 11:44:39 +0800

    安装和引入fortawesome图标库

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue'
+
+library.add(fas)
 
 createApp(App).mount('#app')
