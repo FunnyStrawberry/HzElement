8fac6e75a43cadd7b91aca215b2d84e00c12b8a2
黄智 <1144061645@qq.com>
2025-04-10 21:46:01 +0800

    添加 useClickOutside 函数--判断点击指定Element外部执行对应回调函数

---
+++ src/hooks/use-click-outside.ts
@@ -0,0 +1,20 @@
+const useClickOutside = (
+  elementRef: Ref<null | HTMLElement>,
+  callback: (e: MouseEvent) => void,
+) => {
+  const handler = (e: MouseEvent) => {
+    if (elementRef.value && e.target) {
+      if (!elementRef.value.contains(e.target as HTMLElement)) {
+        callback(e)
+      }
+    }
+  }
+  onMounted(() => {
+    document.addEventListener('click', handler)
+  })
+  onUnmounted(() => {
+    document.removeEventListener('click', handler)
+  })
+}
+
+export default useClickOutside
