c15dce470b57bf33300cf78d6053fe7f383bcf58
黄智 <1144061645@qq.com>
2025-04-15 22:06:53 +0800

    添加按钮基本用法文档

---
+++ docs/demo/button/basic.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import HzButton from '../../../src/components/button/button.vue'
+
+defineOptions({
+  name: 'HzButtonBasic',
+})
+</script>
+
+<template>
+  <hz-button>Default</hz-button>
+  <hz-button type="primary">Primary</hz-button>
+  <hz-button type="success">Success</hz-button>
+  <hz-button type="info">Info</hz-button>
+  <hz-button type="warning">Warning</hz-button>
+  <hz-button type="danger">Danger</hz-button>
+</template>
