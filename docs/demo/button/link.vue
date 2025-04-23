a2277533587746273c2ef5daef48933b579c5b9a
黄智 <1144061645@qq.com>
2025-04-23 19:35:44 +0800

    button 按钮文档--链接按钮

---
+++ docs/demo/button/link.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>基础链接按钮</p>
+  <div class="mb-4">
+    <hz-button link>link</hz-button>
+    <hz-button type="primary" link>primary</hz-button>
+    <hz-button type="success" link>success</hz-button>
+    <hz-button type="info" link>info</hz-button>
+    <hz-button type="warning" link>warning</hz-button>
+    <hz-button type="danger" link>danger</hz-button>
+  </div>
+
+  <p>禁用链接按钮</p>
+  <div>
+    <hz-button link disabled>link</hz-button>
+    <hz-button type="primary" link disabled>primary</hz-button>
+    <hz-button type="success" link disabled>success</hz-button>
+    <hz-button type="info" link disabled>info</hz-button>
+    <hz-button type="warning" link disabled>warning</hz-button>
+    <hz-button type="danger" link disabled>danger</hz-button>
+  </div>
+</template>
