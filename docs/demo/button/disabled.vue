8ad82df318c195aab4016c8d7e81e69ea3e7f4b9
黄智 <1144061645@qq.com>
2025-04-23 12:28:12 +0800

    button 按钮文档--禁用状态

---
+++ docs/demo/button/disabled.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="mb-4">
+    <hz-button disabled>Default</hz-button>
+    <hz-button type="primary" disabled>Primary</hz-button>
+    <hz-button type="success" disabled>Success</hz-button>
+    <hz-button type="info" disabled>Info</hz-button>
+    <hz-button type="warning" disabled>Warning</hz-button>
+    <hz-button type="danger" disabled>Danger</hz-button>
+  </div>
+
+  <div>
+    <hz-button plain disabled>Plain</hz-button>
+    <hz-button type="primary" plain disabled>Primary</hz-button>
+    <hz-button type="success" plain disabled>Success</hz-button>
+    <hz-button type="info" plain disabled>Info</hz-button>
+    <hz-button type="warning" plain disabled>Warning</hz-button>
+    <hz-button type="danger" plain disabled>Danger</hz-button>
+  </div>
+</template>
