2a9b734aba76253f81dc08fdaf03e8e755ce09df
黄智 <1144061645@qq.com>
2025-04-23 22:59:35 +0800

    button 按钮文档--文字按钮

---
+++ docs/demo/button/text.vue
@@ -0,0 +1,31 @@
+<template>
+  <p>基础文字按钮</p>
+  <div class="mb-4">
+    <hz-button text>text</hz-button>
+    <hz-button type="primary" text>primary</hz-button>
+    <hz-button type="success" text>success</hz-button>
+    <hz-button type="info" text>info</hz-button>
+    <hz-button type="warning" text>warning</hz-button>
+    <hz-button type="danger" text>danger</hz-button>
+  </div>
+
+  <p>带背景文字按钮</p>
+  <div class="mb-4">
+    <hz-button text textBg>text</hz-button>
+    <hz-button type="primary" text textBg>primary</hz-button>
+    <hz-button type="success" text textBg>success</hz-button>
+    <hz-button type="info" text textBg>info</hz-button>
+    <hz-button type="warning" text textBg>warning</hz-button>
+    <hz-button type="danger" text textBg>danger</hz-button>
+  </div>
+
+  <p>禁用文字按钮</p>
+  <div>
+    <hz-button text disabled>text</hz-button>
+    <hz-button type="primary" text disabled>primary</hz-button>
+    <hz-button type="success" text disabled>success</hz-button>
+    <hz-button type="info" text disabled>info</hz-button>
+    <hz-button type="warning" text disabled>warning</hz-button>
+    <hz-button type="danger" text disabled>danger</hz-button>
+  </div>
+</template>
