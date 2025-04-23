fcc67e6a4cf9e6cc885b42ef1b1bd56d8a04eb3a
黄智 <1144061645@qq.com>
2025-04-23 11:55:08 +0800

    button 按钮文档--基础用法

--- docs/demo/button/basic.vue
+++ docs/demo/button/basic.vue
@@ -1,16 +1,39 @@
-<script setup lang="ts">
-import HzButton from '../../../src/components/button/button.vue'
-
-defineOptions({
-  name: 'HzButtonBasic',
-})
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <hz-button>Default</hz-button>
-  <hz-button type="primary">Primary</hz-button>
-  <hz-button type="success">Success</hz-button>
-  <hz-button type="info">Info</hz-button>
-  <hz-button type="warning">Warning</hz-button>
-  <hz-button type="danger">Danger</hz-button>
+  <div class="mb-4">
+    <hz-button>Default</hz-button>
+    <hz-button type="primary">Primary</hz-button>
+    <hz-button type="success">Success</hz-button>
+    <hz-button type="info">Info</hz-button>
+    <hz-button type="warning">Warning</hz-button>
+    <hz-button type="danger">Danger</hz-button>
+  </div>
+
+  <div class="mb-4">
+    <hz-button plain>Plain</hz-button>
+    <hz-button type="primary" plain>Primary</hz-button>
+    <hz-button type="success" plain>Success</hz-button>
+    <hz-button type="info" plain>Info</hz-button>
+    <hz-button type="warning" plain>Warning</hz-button>
+    <hz-button type="danger" plain>Danger</hz-button>
+  </div>
+
+  <div class="mb-4">
+    <hz-button round>Round</hz-button>
+    <hz-button type="primary" round>Primary</hz-button>
+    <hz-button type="success" round>Success</hz-button>
+    <hz-button type="info" round>Info</hz-button>
+    <hz-button type="warning" round>Warning</hz-button>
+    <hz-button type="danger" round>Danger</hz-button>
+  </div>
+
+  <div>
+    <hz-button icon="search" circle />
+    <hz-button type="primary" icon="edit" circle />
+    <hz-button type="success" icon="check" circle />
+    <hz-button type="info" icon="envelope" circle />
+    <hz-button type="warning" icon="star" circle />
+    <hz-button type="danger" icon="trash-can" circle />
+  </div>
 </template>
