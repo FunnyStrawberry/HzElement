4c411f4e966842d57cbafa252b0de8b209480a83
黄智 <1144061645@qq.com>
2025-04-24 21:43:20 +0800

    button 按钮文档--按钮组

---
+++ docs/demo/button/group.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mb-4">
+    <hz-button-group type="primary">
+      <hz-button icon="arrow-left">上一页</hz-button>
+      <hz-button icon="arrow-right" iconPosition="right">下一页</hz-button>
+    </hz-button-group>
+
+    <hz-button-group class="ml-4" type="primary" size="default">
+      <hz-button icon="edit" />
+      <hz-button icon="share-nodes" />
+      <hz-button icon="trash-can" />
+    </hz-button-group>
+  </div>
+
+  <div>
+    <hz-button-group class="ml-4" type="primary" size="small">
+      <hz-button icon="edit" />
+      <hz-button type="success" size="default" icon="share-nodes" />
+      <hz-button type="danger" size="large" icon="trash-can" />
+    </hz-button-group>
+  </div>
+</template>
