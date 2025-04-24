07d4386cc04fc1df7b70d3d12702f3512d0eac68
黄智 <1144061645@qq.com>
2025-04-24 23:09:43 +0800

    button 按钮文档--标签

---
+++ docs/demo/button/tag.vue
@@ -0,0 +1,13 @@
+<template>
+  <hz-button>button</hz-button>
+  <hz-button tag="div" role="button" tabindex="0">div</hz-button>
+  <hz-button
+    type="primary"
+    tag="a"
+    href="https://github.com/FunnyStrawberry/HzElement"
+    target="_blank"
+    rhz="noopener noreferrer"
+  >
+    a
+  </hz-button>
+</template>
