b3e9e2afc5a3cd4264af7c1e9c51dd4d5c02deaa
黄智 <1144061645@qq.com>
2025-04-24 21:47:00 +0800

    完善全局注册HzButton插件--将HzButtonGroup加入

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -1,7 +1,11 @@
-import Button from './src/button.vue'
-import { withInstall } from '@hz-element/utils'
 import type { SFCWithInstall } from '@hz-element/utils'
+import { withInstall, withNoopInstall } from '@hz-element/utils'
+import Button from './src/button.vue'
+import ButtomGroup from './src/button-group.vue'
 
-export const HzButton: SFCWithInstall<typeof Button> = withInstall(Button)
+export const HzButton: SFCWithInstall<typeof Button> & {
+  ButtomGroup: typeof ButtomGroup
+} = withInstall(Button, { ButtomGroup })
+export const HzButtonGroup: SFCWithInstall<typeof ButtomGroup> = withNoopInstall(ButtomGroup)
 
 export default HzButton
