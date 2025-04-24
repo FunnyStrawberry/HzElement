5294b1e6d25fb334215d611ce3b9d8ae3141378e
黄智 <1144061645@qq.com>
2025-04-24 22:32:11 +0800

    button 按钮文档--调整尺寸

---
+++ docs/demo/button/size.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="mb-4">
+    <hz-button size="large">Large</hz-button>
+    <hz-button>Default</hz-button>
+    <hz-button size="small">Small</hz-button>
+    <hz-button size="large" icon="search">Search</hz-button>
+    <hz-button icon="search">Search</hz-button>
+    <hz-button size="small" icon="search">Search</hz-button>
+  </div>
+
+  <div class="mb-4">
+    <hz-button size="large" round>Large</hz-button>
+    <hz-button round>Default</hz-button>
+    <hz-button size="small" round>Small</hz-button>
+    <hz-button size="large" icon="search" round>Search</hz-button>
+    <hz-button icon="search" round>Search</hz-button>
+    <hz-button size="small" icon="search" round>Search</hz-button>
+  </div>
+
+  <div>
+    <hz-button icon="search" size="large" circle />
+    <hz-button icon="search" circle />
+    <hz-button icon="search" size="small" circle />
+  </div>
+</template>
