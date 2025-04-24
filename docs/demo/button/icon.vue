1b9e20889108d64d46c130b6a62eb33003a25998
黄智 <1144061645@qq.com>
2025-04-24 19:28:15 +0800

    button 按钮文档--图标按钮

---
+++ docs/demo/button/icon.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <hz-button type="primary" icon="edit" />
+    <hz-button type="primary" icon="share-nodes" />
+    <hz-button type="primary" icon="trash-can" />
+    <hz-button type="primary" icon="search">Search</hz-button>
+    <hz-button type="primary" icon="upload" iconPosition="right">Upload</hz-button>
+  </div>
+</template>
