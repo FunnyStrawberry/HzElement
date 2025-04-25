35c399393f0879ef2b73c84eb82f3ebcec256f24
黄智 <1144061645@qq.com>
2025-04-25 21:45:48 +0800

    button 按钮文档--自定义颜色

---
+++ docs/demo/button/custom.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mb-4">
+    <hz-button color="#626aef">Default</hz-button>
+    <hz-button color="#626aef" plain>Plain</hz-button>
+    <hz-button color="#626aef" disabled>Disabled</hz-button>
+    <hz-button color="#626aef" disabled plain>Disabled Plain</hz-button>
+  </div>
+
+  <div>
+    <hz-button color="#626aef" link>Link</hz-button>
+    <hz-button color="#626aef" disabled link>Disabled Link</hz-button>
+    <hz-button color="#626aef" text>Text</hz-button>
+    <hz-button color="#626aef" text textBg>Text TextBg</hz-button>
+    <hz-button color="#626aef" disabled text>Disabled Text</hz-button>
+    <hz-button color="#626aef" disabled text textBg>Disabled Text TextBg</hz-button>
+  </div>
+</template>
